613f571ee8081f1705cd43cbd9618425375644a9
koachzz <1106108114@nkust.edu.tw>
2020-06-10 03:04:26 +0800

    20200610 Edit added net.py and update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,30 +1,28 @@
 from tkinter import *
-
+import os
 class Gui_For_Office:
 
     def __init__(self,master):
         self.master = master
         self.initUI()
 
-    def start_page(self):
-        pass
+    def to_start_page(self):
+        self.questions_page.pack_forget()
+        self.start_page.pack(expand=True, fill=BOTH)
 
-    def questions_page(self):
-        pass
 
-    def click_ok(self):
+    def to_questions_page(self):
         self.start_page.pack_forget()
-        self.test_btn = Button()
+        self.questions_page.pack(expand=True, fill=BOTH)
 
 
     def initUI(self):
         #set UI 
-        self.master.geometry("300x300")
+        self.master.geometry("1920x1080")
         self.master.title("Office Guidance System")
-        self.start_page()
 
         #start page setting
-        self.start_page = Frame(self.master, width=300, height=300, bg="gray")
+        self.start_page = Frame(self.master, width=1920, height=1080, bg="gray")
         start_page_middle = Frame(self.start_page,bg = "white")
 
         #Label of notice
@@ -38,7 +36,7 @@ class Gui_For_Office:
         std_id.pack()
 
         #middle widget
-        btn_OK = Button(start_page_middle,text = "OK",width = 10,command = self.click_ok)
+        btn_OK = Button(start_page_middle,text = "OK",width = 10,command = self.to_questions_page)
         btn_OK.pack()
 
         #middle widget
@@ -47,12 +45,32 @@ class Gui_For_Office:
 
         #middle frame setting
         start_page_middle.place(relx=0.5, rely=0.5, anchor=CENTER)
-        
-        #start page setting
-        self.start_page.pack(expand=True, fill=BOTH)
+
+
+        #qeustions page setting
+        self.questions_page = Frame(self.master, width=1920, height=1080, bg="green")
+
+        questions_label_var = StringVar()
+        questions_label = Label(self.questions_page,font = ("微軟正黑體",30), textvariable = questions_label_var)
+        questions_label_var.set("請選擇欲服務項目")
+        questions_label.pack(anchor="n", side = TOP)
+
+        buttons_frame = Frame(self.questions_page)
+        buttons_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+        services_cnt = 9
+        for i in range(services_cnt//3):
+            for j in range(3):
+                Button(buttons_frame,text = "test",width = 50,height = 10).grid(row = i, column = j, padx = 100, pady = 60)
+
+        btn_cancel = Button(self.questions_page,text = "取消/上一頁", font =("微軟正黑體",30),command = self.to_start_page)
+        btn_cancel.pack(anchor="n", side =LEFT)
+                
+
+
 
 
 
 if __name__ == "__main__":
     a = Gui_For_Office(Tk())
+    a.to_start_page()
     a.master.mainloop()
---
+++ net_readme.txt
@@ -0,0 +1,4 @@
+host(data 資料, staff_branch 分機號碼)  call host 給要傳輸的資料和 要給哪位的分機號碼
+
+staff(host , port )  員工電腦的(host , port )  一開始binding後會等待host送資料來，接受到資料後會關閉，需再呼叫一次
+
